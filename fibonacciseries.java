13c390f8b7e48e9a477cbbc43500a8bbf04d68af
naresh <lavetinaresh3@gmail.com>
2025-03-26 18:54:45 +0530

    number

--- fibonacciseries.java
+++ fibonacciseries.java
@@ -1,11 +1,11 @@
 public class fibonacciseries {
     public static void main(String[] args){
-        int f1=1;
-        int f2=2;
+        int f1=0;
+        int f2=1;
         int f3;
         System.out.println(f1+" ");
         System.out.println(f2+" ");
-        for(int i=1; i<=7; i++)
+        for(int i=0;i<7; i++)
         {
             f3=f1+f2;
             System.out.println(f3+ " ");
